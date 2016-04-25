7e22ab04b91762b33b9375d922c0e305031d49ff
Alexandre Carneiro <alexcarneiro1993@gmail.com>
2016-04-25 06:15:57 -0400

    Added fields First name and Last name to registration form and added logo/slogan to forms

--- Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/models/user.rb
+++ Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_presence_of :first_name, :last_name
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+          :trackable, :validatable
+
+
 end
