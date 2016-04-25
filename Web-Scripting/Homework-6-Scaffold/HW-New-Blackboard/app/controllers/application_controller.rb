b5454101b490f7f4a20e8b571a82f47f5c9b68ff
Alexandre Carneiro <alexcarneiro1993@gmail.com>
2016-04-25 15:58:53 -0400

    Changed logo colorand setting and created header with logo and user name + logout

--- Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/controllers/application_controller.rb
+++ Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
  def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-     user_params.permit({ roles: [:professor, :student] }, :email, :password, :password_confirmation)
+     user_params.permit({ roles: [:professor] }, :email, :password, :password_confirmation)
    end
  end
 
--- Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/models/user.rb
+++ Web-Scripting/Homework-6-Scaffold/HW-New-Blackboard/app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
           :trackable, :validatable
 
-
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 end
